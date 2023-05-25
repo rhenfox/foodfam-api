ea864fbfc7b4482899f8309cc1fe87ffaea22f76
aldri <aldri@ALDRIN>
2023-05-25 10:00:04 +0800

    FoodFam-API

--- src/main/java/com/aldrin/foodfam/controller/UserProfileController.java
+++ src/main/java/com/aldrin/foodfam/controller/UserProfileController.java
@@ -4,7 +4,6 @@
  */
 package com.aldrin.foodfam.controller;
 
-import com.aldrin.foodfam.model.User;
 import com.aldrin.foodfam.model.UserProfile;
 import com.aldrin.foodfam.service.UserProfileService;
 //import jakarta.validation.Valid;
@@ -13,15 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 /**
  * @author ALDRIN
  */
