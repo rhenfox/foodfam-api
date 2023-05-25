c7eb7e12aae7e0648853a0125555e03454e54d29
aldri <aldri@ALDRIN>
2023-05-25 09:36:12 +0800

    FoodFam-API

---
+++ src/main/java/com/aldrin/foodfam/model/UsedIngredient.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aldrin.foodfam.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.ArrayList;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author ALDRIN
+ */
+//@Setter
+//@Getter
+//@ToString
+//@Entity
+public class UsedIngredient {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+    public double amount;
+    public String unit;
+    public String unitLong;
+    public String unitShort;
+    public String aisle;
+    public String name;
+    public String original;
+    public String originalName;
+    public ArrayList<String> meta;
+    public String image;
+    public String extendedName;
+}
